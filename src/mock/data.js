070d3fc4f8e5d9cfd41f0c2e69b487127bfc9b0f
Bebop <nicolas.bibboperez@gmail.com>
2020-10-05 18:29:29 -0300

    Added Firebase hosting and modified with own data

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,64 +2,50 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: '<bebop />', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Software Developer Portfolio', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hi, this is',
+  name: '<bebop />',
+  subtitle: 'Your next software partner',
+  cta: "Let's meet",
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'profile.png',
+  paragraphOne:
+    "My real name is Nicolás Bibbó Pérez, I'm a software developer based in Mar del Plata, Buenos Aires, Argentina.",
+  paragraphTwo:
+    'This portfolio is meant to showcase my latest projects, so you can get an idea of what we can build together.',
+  paragraphThree: 'Know more about my skills and experience checking my resume right here:',
+  resume:
+    'https://firebasestorage.googleapis.com/v0/b/nico-bebop.appspot.com/o/Nicolas-Bibbo-Resume.pdf?alt=media', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'project1.jpg',
+    title: 'Módulo Renders',
+    info:
+      'Online portfolio for a 3d architecture rendering project. It consists of a photo gallery of renders, with an admin section for CRUD and a simple contact form.',
+    info2: 'Built with React and Firebase. [Currently under development]',
+    url: 'https://modulorenders.web.app/',
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
   cta: '',
-  btn: '',
-  email: '',
+  btn: 'Get in touch',
+  email: 'nicolas.bibboperez@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,28 +53,18 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://linkedin.com/in/nico-bebop',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/nico-bebop',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import App from '../components/App';
 import { headData } from '../mock/data';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/main.scss';
+import Firebase, { FirebaseContext } from '../components/Firebase';
 
 export default () => {
   const { title, lang, description } = headData;
@@ -16,7 +17,9 @@ export default () => {
         <html lang={lang || 'en'} />
         <meta name="description" content={description || 'Gatsby Simplefolio'} />
       </Helmet>
-      <App />
+      <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+      </FirebaseContext.Provider>
     </>
   );
 };
